2d2ac768e8e05382ed478d7716509143601fadb1
Jean Arreola <jean.arreola@yahoo.com.mx>
2017-07-03 00:44:28 -0500

    Luigi pipeline added

---
+++ luigi_pipeline.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 03 00:23:22 2017
+
+@author: Jean
+"""
+
+import luigi
+import subprocess
+ 
+class CreateTable(luigi.Task):
+ 
+    def requires(self):
+        return []
+ 
+    def output(self):
+        return []
+ 
+    def run(self):
+        subprocess.call('Rscript create_table.R', shell = True)
+        
+        
+if __name__ == '__main__':
+    luigi.run()
